83bc12c32d0301fa119ecb58395e7a3220bbbe80
damian sola <damiansola99@gmail.com>
2025-05-23 17:08:22 -0300

    estilos casi listos 2

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,10 +1,10 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types';
-import { Text ,StyleSheet} from 'react-native';
 import HomeScreen from '../screens/HomeScreen';
 import FormScreen from '../screens/FormScreen';
 import ResultScreen from '../screens/ResultScreen';
-
+import { MaterialIcons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
 // importar screens
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -12,9 +12,24 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export const AppNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Home" >
-        <Stack.Screen name='Home' component={HomeScreen}/>
-        <Stack.Screen name="Form" component={FormScreen} />
-        <Stack.Screen name="Result" component={ResultScreen} />
+        <Stack.Screen name='Home' component={HomeScreen}
+         options={{
+          title: 'Inicio',
+          headerLeft: () => (
+            <TouchableOpacity style={{ marginLeft: 10 }}>
+              <MaterialIcons name="home" size={30} color="black" />
+            </TouchableOpacity>
+          ),
+        }}/>
+        <Stack.Screen name="Form" component={FormScreen} 
+        options={{
+          title: 'Generando promp',
+        }}
+        />
+        <Stack.Screen name="Result" component={ResultScreen} 
+         options={{
+          title: 'Frase generada',
+        }}/>
     </Stack.Navigator>
   );
 };
